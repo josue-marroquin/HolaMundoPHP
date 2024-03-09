fd0b79d82d82a55c624c1bfb315d5927ca856149
Josue M <joshx210@gmail.com>
2024-03-09 17:52:00 -0600

    Comint inicial de Hola Mundo

---
+++ holaMundo.php
@@ -0,0 +1,11 @@
+<body style="background-color:black; color:white;">
+<?php
+    echo "<h3><strong>Hola Mundo!</strong>, soy <i>Josue Marroquin</i></h3>";
+?>
+<p><strong>Prompt utilizado:</strong><br>
+An image depicting a world dominated by machines<br>
+where robots of various types inhabit a city interlaced with circuits<br>
+all orchestrated towards galactic domination through a Dyson sphere.
+</p>
+<img src='DysonSphere.jpg' alt="Machine World" height='700' width='700'></img>
+</body>
